2bf0642fe2d18888efb71e3c5154de851c40f844
jhordycamacas <jhordycamacas9@gmail.com>
2024-12-18 11:37:50 -0500

    cambios

--- Ejemplosc3/src/paquete01/Ejemplo021.java
+++ Ejemplosc3/src/paquete01/Ejemplo021.java
@@ -17,25 +17,26 @@ public class Ejemplo021 {
     public static void main(String[] args) {
         // Creación de arreglo bidimensionales
         String cadenaFinal = "";
+
         // forma 1
         int[][] arreglo1 = new int[3][4]; //
         // System.out.printf("Uso de la opción length: %d\n", arreglo1.length);
-        
+
         // System.out.printf("Valor en la posición [0][2]\n", arreglo1[0][2]);
-       
         arreglo1[0][2] = 1000;
-          
+
         for (int fila = 0; fila < arreglo1.length; fila++) {
             for (int col = 0; col < arreglo1[fila].length; col++) {
                 // System.out.printf("fila[%d] columna[%d] = %d\n", 
                 //        fila, col, arreglo1[fila][col]);
-                cadenaFinal = String.format("%sfila[%d] columna[%d] = %d\n", 
+                cadenaFinal = String.format("%sfila[%d] columna[%d] = %d\n",
                         cadenaFinal, fila, col, arreglo1[fila][col]);
             }
+            cadenaFinal = String.format("%s-------------------\n", cadenaFinal);
         }
-        
+
         System.out.printf("%s\n", cadenaFinal);
-        
+
     }
-    
+
 }
